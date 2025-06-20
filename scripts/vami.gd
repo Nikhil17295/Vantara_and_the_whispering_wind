a8889c03beff28580d1fb851c73500572a60f80e
Nikhil17295 <nikhil.bansal123451@gmail.com>
2025-06-20 22:31:04 +0530

    camera work changed
    smooth transition

--- scripts/vami.gd
+++ scripts/vami.gd
@@ -1,23 +1,23 @@
 extends CharacterBody2D
 
 # Movement speeds
-const SPEED = 280.0
+const SPEED = 270.0
 const MAX_FALL_SPEED = 800.0
 
 # Jump forces
 const JUMP_VELOCITY = -470.0
 const GRAVITY = 1100.0
-const FALL_GRAVITY_MULTIPLIER = 1.6
-const SHORT_JUMP_MULTIPLIER = 2.4
+const FALL_GRAVITY_MULTIPLIER = 1.5
+const SHORT_JUMP_MULTIPLIER = 2.3
 
 # Timings
-const COYOTE_TIME = 0.1
-const JUMP_BUFFER_TIME = 0.1
+const COYOTE_TIME = 0.08
+const JUMP_BUFFER_TIME = 0.08
 
 # Acceleration
 const ACCELERATION = 2100.0
-const FRICTION = 1700.0
-const AIR_ACCELERATION = 1200.0
+const FRICTION = 1800.0
+const AIR_ACCELERATION = 1400.0
 const AIR_FRICTION = 700.0
 
 # Apex hang
@@ -25,92 +25,89 @@ const APEX_HANG_VELOCITY = 30.0
 const APEX_GRAVITY_MULTIPLIER = 0.5
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var ray_left: RayCast2D = $Node2D/Rayleft
-@onready var ray_center: RayCast2D = $Node2D/Raycenter
-@onready var ray_right: RayCast2D = $Node2D/Rayright
+@onready var camera_target: Marker2D = $CameraTarget
 
-
-var jump_buffer=0.0
-var coyote_time:float=0.0
-var direction : float = 0.0
-var grounded : bool = true
+# State variables
+var jump_buffer = 0.0
+var coyote_time: float = 0.0
+var direction: float = 0.0
 
 func _ready():
 	animated_sprite.play("idle")
-	
-	# used physics process to execute my functions as awhole while making the functions seperately 
+
+func _process(delta: float) -> void:
+	var target_position = global_position
+	camera_target.global_position = camera_target.global_position.lerp(target_position, delta * 15.0)
+	camera_target.global_position = camera_target.global_position.snapped(Vector2(1, 1)) # snap to pixel grid
+
+
 func _physics_process(delta: float) -> void:
-	direction = Input.get_axis("move_left","move_right")
-	grounded = is_grounded()
-	handle_horizontal_movement(delta)
-	handle_animation()
-	handle_gravity_and_jump(delta)
+	direction = Input.get_axis("move_left", "move_right")
+	var grounded: bool = is_on_floor()
+
+	handle_horizontal_movement(delta, grounded)
+	handle_gravity_and_jump(delta, grounded)
+	handle_animation(grounded)
+
 	move_and_slide()
 
-	# making the custom grounded func instead of is on floor
-func is_grounded() -> bool:
-	return ray_left.is_colliding() or ray_center.is_colliding() or ray_right.is_colliding()
-	
-func handle_horizontal_movement(delta : float) -> void:
-	# making horizontal movement
+func handle_horizontal_movement(delta: float, grounded: bool) -> void:
 	if grounded:
-	# Ground movement
 		if direction != 0:
 			velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION * delta)
 		else:
 			velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 	else:
-	# Air movement
 		if direction != 0:
 			velocity.x = move_toward(velocity.x, direction * SPEED, AIR_ACCELERATION * delta)
 		else:
 			velocity.x = move_toward(velocity.x, 0, AIR_FRICTION * delta)
 
-func handle_animation():
-	# need to add sprite animations in it
-	if direction != 0:
-		animated_sprite.play("idle")
-		animated_sprite.flip_h = direction < 0
+func handle_gravity_and_jump(delta: float, grounded: bool) -> void:
+	# Coyote time
+	if grounded:
+		coyote_time = COYOTE_TIME
 	else:
-		animated_sprite.play("idle")
-		
-		
-	# for the jump, basic
-func handle_gravity_and_jump(delta) -> void:
-	
-	# in not grounded section:
-	#    there are gravity controls 
-	
-	if not grounded:
 		coyote_time -= delta
-		# apex hanging of the jump
-		if abs(velocity.y) < APEX_HANG_VELOCITY :
-			velocity.y += GRAVITY * delta * APEX_GRAVITY_MULTIPLIER
+
+	# Jump buffer
+	if Input.is_action_just_pressed("jump"):
+		jump_buffer = JUMP_BUFFER_TIME
+	else:
+		jump_buffer -= delta
+
+	# Gravity
+	if not grounded:
+		if abs(velocity.y) < APEX_HANG_VELOCITY:
+			velocity.y += GRAVITY * APEX_GRAVITY_MULTIPLIER * delta
+		elif velocity.y > 0:
+			velocity.y += GRAVITY * FALL_GRAVITY_MULTIPLIER * delta
+		elif velocity.y < 0 and not Input.is_action_pressed("jump"):
+			velocity.y += GRAVITY * SHORT_JUMP_MULTIPLIER * delta
 		else:
 			velocity.y += GRAVITY * delta
 		
-			
-			if velocity.y > 0:
-				velocity.y += GRAVITY * (FALL_GRAVITY_MULTIPLIER - 1) * delta
-			elif velocity.y < 0 and not Input.is_action_pressed("jump"):
-				velocity.y += GRAVITY * (SHORT_JUMP_MULTIPLIER - 1) * delta
-		velocity.y = min( velocity.y , MAX_FALL_SPEED )
-
-
-	else: # this is the grounded section
-		if velocity.y > 0: # Only reset if not in jump
+		velocity.y = min(velocity.y, MAX_FALL_SPEED)
+	else:
+		if velocity.y > 0:
 			velocity.y = 0
-		coyote_time = COYOTE_TIME
 
-
-	  # this here is for jump buffer and with this there is no need to check many times about the jump is pressed or not
-	if Input.is_action_just_pressed("jump"):
-		jump_buffer=JUMP_BUFFER_TIME
-	else :
-		jump_buffer -= delta
-		
-	 # this is the main code that executes the jump
+	# Jump execution
 	if jump_buffer > 0 and (grounded or coyote_time > 0):
 		velocity.y = JUMP_VELOCITY
-		coyote_time = 0.0  
 		jump_buffer = 0.0
+		coyote_time = 0.0
+
+func handle_animation(grounded: bool) -> void:
+	if not grounded:
+		if velocity.y < 0:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("fall")
+	elif abs(velocity.x) > 10:
+		animated_sprite.play("run")
+	else:
+		animated_sprite.play("idle")
+
+	if direction != 0:
+		animated_sprite.flip_h = direction < 0
