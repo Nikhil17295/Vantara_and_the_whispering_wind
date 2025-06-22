013ae1f67913dec4207ff31aa57ac541cdd90297
Nikhil17295 <nikhil.bansal123451@gmail.com>
2025-06-22 14:22:00 +0530

    basic tunning and going for a fine change but not sure will happen

--- scripts/vami.gd
+++ scripts/vami.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 # Movement speeds
-const SPEED = 270.0
+const SPEED = 280.0
 const MAX_FALL_SPEED = 750.0
+const AIR_BOOST_SPEED = 200.0
 
 # Jump forces
 const JUMP_VELOCITY = -470.0
@@ -15,10 +16,10 @@ const COYOTE_TIME = 0.1
 const JUMP_BUFFER_TIME = 0.1
 
 # Acceleration
-const ACCELERATION = 1500.0
-const FRICTION = 1800.0
-const AIR_ACCELERATION = 1400.0
-const AIR_FRICTION = 700.0
+const ACCELERATION = 800.0
+const FRICTION = 2200.0
+const AIR_ACCELERATION = 1500.0
+const AIR_FRICTION = 1700.0
 
 # Apex hang
 const APEX_HANG_VELOCITY = 30.0
@@ -30,6 +31,8 @@ const APEX_GRAVITY_MULTIPLIER = 0.5
 var jump_buffer = 0.0
 var coyote_time: float = 0.0
 var direction: float = 0.0
+var grounded: bool = is_on_floor()
+var last_air_input_direction = 0
 
 func _ready():
 	animated_sprite.play("idle")
@@ -38,43 +41,38 @@ func _ready():
 @onready var camera = $Camera2D
 var cam_offset := Vector2.ZERO
 var facing_direction := 1
-const LOOKAHEAD_ZONE := 50
-var vertical_target = -10
+const LOOKAHEAD_ZONE := 60.0
+var vertical_target = -20.0
+var horizontal_target = 0.0
 
 func _process(delta):
 	if direction != 0:
 		facing_direction = direction
-
-	# Only activate lookahead if player is near center
-	var player_offset = global_position.x - camera.global_position.x
-
-	if abs(player_offset) <= LOOKAHEAD_ZONE:
-		cam_offset.x = (LOOKAHEAD_ZONE * facing_direction) + player_offset
-	else:
-		cam_offset.x = 0  # Freeze when off-center
+		
+	if grounded :
+		horizontal_target = LOOKAHEAD_ZONE * facing_direction
+	if not grounded:
+		cam_offset.x = horizontal_target
 
 	# Vertical look target
 	if velocity.y >= MAX_FALL_SPEED:
-		vertical_target = 110.0
-	elif Input.is_action_pressed("look_up"):
-		vertical_target = -80.0
-	elif Input.is_action_pressed("look_down"):
-		vertical_target = 90.0
+		vertical_target = 120.0
+	elif grounded and Input.is_action_pressed("look_up"):
+		vertical_target = -70.0
+	elif grounded and Input.is_action_pressed("look_down"):
+		vertical_target = 100.0
 	else:
-		vertical_target = -10.0
+		vertical_target = 20.0
 
-	# Apply vertical smoothing to cam_offset.y, not directly to camera
-	cam_offset.y = move_toward(cam_offset.y, vertical_target, delta * 250.0)
-
-	# Apply full smoothed offset to camera
-	camera.offset = camera.offset.move_toward(cam_offset, delta * 180.0)
-
+	cam_offset.y = move_toward(cam_offset.y, vertical_target, delta * 180.0)
+	cam_offset.x = move_toward(cam_offset.x, horizontal_target , delta * 320.0)
+	camera.offset = cam_offset
 
 func _physics_process(delta: float) -> void:
 	direction = Input.get_axis("move_left", "move_right")
 	if direction != 0:
 		facing_direction = direction
-	var grounded: bool = is_on_floor()
+	grounded = is_on_floor()
 
 	handle_horizontal_movement(delta, grounded)
 	handle_gravity_and_jump(delta, grounded)
@@ -89,7 +87,11 @@ func handle_horizontal_movement(delta: float, grounded: bool) -> void:
 		else:
 			velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 	else:
-		if direction != 0:
+		if direction != 0 and direction != last_air_input_direction:
+			velocity.x += AIR_BOOST_SPEED * direction
+			last_air_input_direction = direction
+
+		elif direction != 0:
 			velocity.x = move_toward(velocity.x, direction * SPEED, AIR_ACCELERATION * delta)
 		else:
 			velocity.x = move_toward(velocity.x, 0, AIR_FRICTION * delta)
