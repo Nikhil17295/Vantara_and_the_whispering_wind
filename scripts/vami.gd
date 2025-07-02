da8752b357c2f0d1031f39a31ef98e56c25b8531
Nikhil17295 <nikhil.bansal123451@gmail.com>
2025-07-02 19:48:21 +0530

    slight changes and still under work

--- scripts/vami.gd
+++ scripts/vami.gd
@@ -25,9 +25,7 @@ const AIR_FRICTION = 2000.0
 const APEX_HANG_VELOCITY = 30.0
 const APEX_GRAVITY_MULTIPLIER = 0.5
 
-#camera delays
-const VERTICAL_FOLLOW_DELAY := 0.15
-const LOOKAHEAD_DELAY := 0.5
+
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -35,61 +33,14 @@ const LOOKAHEAD_DELAY := 0.5
 var jump_buffer = 0.0
 var coyote_time: float = 0.0
 var direction: float = 0.0
-var grounded: bool = is_on_floor()
+var grounded: bool = false
 var last_air_input_direction = 0
 
 func _ready():
 	animated_sprite.play("idle")
 	get_viewport().size = DisplayServer.screen_get_size()
 
-@onready var camera = $Camera2D
-var cam_offset := Vector2.ZERO
-const LOOKAHEAD_ZONE := 40.0
-var vertical_target = -20.0
-var horizontal_target = 0.0
-var lookahead_delay_timer := 0.0
-var last_direction := 0
-var vertical_follow_timer := 0.0
-var last_vertical_pos := 0.0
-var camera_y_locked := true
-
-
-
-func _process(delta):
-	# Only change facing direction if input is consistent for some time
-	if direction != 0:
-		if direction == last_direction and sign(velocity.x) == direction:
-			lookahead_delay_timer += delta
-		else:
-			lookahead_delay_timer = 0.0
-			last_direction = direction
-
-		if lookahead_delay_timer >= LOOKAHEAD_DELAY:
-			last_direction = direction
-	else:
-		# Reset delay if no direction pressed
-		lookahead_delay_timer = 0.0
-
-	# Now apply lookahead only after facing direction is updated
-	if grounded:
-		horizontal_target = LOOKAHEAD_ZONE * last_direction
-
-	# Vertical look target
-	if velocity.y >= MAX_FALL_SPEED:
-		camera_y_locked = false
-		vertical_target = 120.0
-	elif grounded and Input.is_action_pressed("look_up"):
-		vertical_target = -60.0
-	elif grounded and Input.is_action_pressed("look_down"):
-		vertical_target = 80.0
-	else:
-		vertical_target = 20.0
-	if not camera_y_locked :
-		cam_offset.y = move_toward(cam_offset.y, vertical_target, delta * 140.0)
-	cam_offset.x = move_toward(cam_offset.x, horizontal_target , delta * 240.0)
-	if (camera.offset - cam_offset).length() > 2.0:
-		camera.offset = camera.offset.move_toward(cam_offset, delta * 150.0)
-
+var last_direction := 0.0
 
 func _physics_process(delta: float) -> void:
 	grounded = is_on_floor()
@@ -98,27 +49,13 @@ func _physics_process(delta: float) -> void:
 		if direction != 0 and (sign(velocity.x) == direction or velocity.x == 0):
 			last_direction = direction
 
-	handle_horizontal_movement(delta, grounded)
-	handle_gravity_and_jump(delta, grounded)
-	handle_animation(grounded)
+	handle_horizontal_movement(delta)
+	handle_gravity_and_jump(delta)
+	handle_animation()
 
 	move_and_slide()
 	
-	#for camera_y_locked
-	var vertical_movement = abs(global_position.y - last_vertical_pos)
-
-	if vertical_movement > 2.0:  # Player moved vertically
-		vertical_follow_timer = 0.0
-		camera_y_locked = true
-	else:
-		vertical_follow_timer += delta
-		if vertical_follow_timer >= VERTICAL_FOLLOW_DELAY:
-			camera_y_locked = false
-
-	last_vertical_pos = global_position.y
-
-
-func handle_horizontal_movement(delta: float, grounded: bool) -> void:
+func handle_horizontal_movement(delta: float) -> void:
 	if grounded:
 		last_air_input_direction = direction
 		if direction != 0:
@@ -135,7 +72,7 @@ func handle_horizontal_movement(delta: float, grounded: bool) -> void:
 		else:
 			velocity.x = move_toward(velocity.x, 0, AIR_FRICTION * delta)
 
-func handle_gravity_and_jump(delta: float, grounded: bool) -> void:
+func handle_gravity_and_jump(delta: float) -> void:
 	# Coyote time
 	if grounded:
 		coyote_time = COYOTE_TIME
@@ -171,14 +108,14 @@ func handle_gravity_and_jump(delta: float, grounded: bool) -> void:
 		jump_buffer = 0.0
 		coyote_time = 0.0
 
-func handle_animation(grounded: bool) -> void:
+func handle_animation() -> void:
 	if not grounded:
 		if velocity.y < 0:
-			animated_sprite.play("jump")
+			animated_sprite.play("idle")
 		else:
-			animated_sprite.play("fall")
+			animated_sprite.play("idle")
 	elif abs(velocity.x) > 10:
-		animated_sprite.play("run")
+		animated_sprite.play("idle")
 	else:
 		animated_sprite.play("idle")
 
